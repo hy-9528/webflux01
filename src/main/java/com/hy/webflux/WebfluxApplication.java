c69e9e8738b5af6e22a43f2a6d072195a18c1245
hy <hy@qq.com>
2022-03-24 20:40:11 +0800

    "second commit"

--- src/main/java/com/hy/webflux/WebfluxApplication.java
+++ src/main/java/com/hy/webflux/WebfluxApplication.java
@@ -2,12 +2,13 @@ package com.hy.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WebfluxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebfluxApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(WebfluxApplication.class, args);
     }
 
 }
